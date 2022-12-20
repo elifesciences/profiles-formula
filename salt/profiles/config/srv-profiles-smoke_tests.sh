f41a7eb7e10fdb5d83b14cee11337acadf03871f
Luke <lsh-0@users.noreply.github.com>
2022-12-20 11:08:29 +1030

    hack

--- salt/profiles/config/srv-profiles-smoke_tests.sh
+++ salt/profiles/config/srv-profiles-smoke_tests.sh
@@ -3,11 +3,12 @@ set -e
 . /opt/smoke.sh/smoke.sh
 
 cd /home/{{ pillar.elife.deploy_user.username }}/profiles
-# TODO: needs new version of self-reliant smoke tests that do not require parameters
-#docker-compose exec wsgi ./smoke_tests_wsgi.sh
 
 set +e
 smoke_url_ok localhost/ping
+
+sleep 5
+
 smoke_url_ok localhost/profiles
 smoke_report
 
